beb62c9035088debb1587fc629dc3ee84e07120d
Pim van den Toorn <pimvdtoorn@hotmail.com>
2024-06-11 15:50:58 +0200

    Added min_distance between moves

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from PathFinder import get_possible_paths, reduce_path, pathfind, first_intersection
+from math import sqrt
+
+from MathHelper import distance_point_to_point, line_intersection, line_intersection_t, line_intersection_t_u
+from PathFinder import pathfind
 from Types import L, P
-from objects.Move import Move, steps_str
+from objects.Move import Move, steps_str, min_distance
 from objects.Field import Field
-from MathHelper import Line, line_intersection, distance_point_to_point
 from objects.Obstacle import Obstacle
 from objects.Robot import Robot
 
@@ -24,10 +26,10 @@ end = P(6, 5)
 line = L(start, end)
 move = Move(line, clearance)
 
-path = pathfind(move, field)
-print(steps_str(path))
-print(path)
-print(path[-1].end_time)
+# path = pathfind(move, field)
+# print(steps_str(path))
+# print(path)
+# print(path[-1].end_time)
 
 
 # paths = get_possible_paths(move, field)
@@ -47,3 +49,29 @@ print(path[-1].end_time)
 
 # angle to side of obstacle
 # Direct routes and single steps
+
+m1 = Move(L(2, 3, 1, 0), 0.2, 0, 0.001)
+m2 = Move(L(2, 0, 0, 3), 0.2, 4000, 0.002)
+
+min_dist, t = min_distance(m1, m2)
+print(f"min_dist: {min_dist}, t: {t}")
+
+
+# def tr(m: Move):
+#     return lambda t: t/m.line.len
+#
+#
+# def pr(m: Move):
+#     return lambda t: m.start + (m.end-m.start) * (t/m.line.len)
+#
+#
+# def dt(m1: Move, m2: Move):
+#     # p1x = m1.x1 + (m1.x2-m1.x1) * (t/m1.line.len)
+#     # p1y = m1.y1 + (m1.y2-m1.y1) * (t/m1.line.len)
+#     # p2x = m2.x1 + (m2.x2-m2.x1) * (t/m2.line.len)
+#     # p2y = m2.y1 + (m2.y2-m2.y1) * (t/m2.line.len)
+#     return lambda t: distance_point_to_point(pr(m1)(t), pr(m2)(t))
+
+
+
+
--- objects/Move.py
+++ objects/Move.py
@@ -1,20 +1,17 @@
-from math import ceil
-
-from Types import Line
+from MathHelper import line_intersection_t_u, distance_point_to_point
+from Types import Line, Point
 
 
 class Move:
-    def __init__(self, line: Line, clearance: float, start_time=0, end_time=0):
+    def __init__(self, line: Line, clearance: float, start_time: float = 0.0, speed: float = 0.001):
         self.x1 = line.x1
         self.y1 = line.y1
         self.x2 = line.x2
         self.y2 = line.y2
         self.line = line
         self.start_time = start_time
-        if end_time == 0:
-            self.end_time = start_time + ceil(self.line.len*1000)
-        else:
-            self.end_time = end_time
+        self.speed = speed
+        self.end_time = start_time + self.line.len/speed
 
         self.clearance = clearance
         self.x_min = min(self.x1, self.x2) - clearance
@@ -43,6 +40,15 @@ class Move:
     def end(self):
         return self.line.p2
 
+    def location_at_time(self, time: float) -> Point:
+        if self.end_time < time:
+            return Point(float('inf'), float('inf'))
+        if self.start_time > time:
+            return Point(float('inf'), float('inf'))
+
+        path_completion_level = (time - self.start_time) / (self.end_time - self.start_time)
+        return self.start + (self.end - self.start) * path_completion_level
+
 
 # Used to copy into https://www.Desmos.com/calculator
 def steps_str(moves: list[Move]) -> str:
@@ -52,3 +58,13 @@ def steps_str(moves: list[Move]) -> str:
     for move in moves:
         string += ',' + move.end.bare_str()
     return string
+
+
+def min_distance(m1: Move, m2: Move) -> tuple[float, float]:
+    t, u = line_intersection_t_u(m1.line, m2.line)
+    time1 = t * m1.line.len / m1.speed + m1.start_time
+    time2 = u * m2.line.len / m2.speed + m2.start_time
+    t_avg = (time1 * m1.speed + time2 * m2.speed) / (m1.speed + m2.speed)
+    p1 = m1.location_at_time(t_avg)
+    p2 = m2.location_at_time(t_avg)
+    return distance_point_to_point(p1, p2), t_avg
